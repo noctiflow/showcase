ebca160cdb55c929c8c793401fd379f5998dcbe4
Richard Wang <rygoon@gmail.com>
2017-10-06 17:23:50 -0700

    change things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require bootstrap-sprockets
 //= require materialize
 //= require_tree .
+
+$(document).ready (function(){
+  $("#warn-alert").fadeTo(2000, 500).slideUp(800, function(){
+    $("#warn-alert").slideUp(500);
+    });
+  $("#success-alert").fadeTo(2000, 500).slideUp(800, function(){
+    $("#success-alert").slideUp(500);
+});
+
+ });
